ddb04a32337bbbbbf623c04ee42c87ec23dbfa26
Ainn Asano <121272813+TheLordNecromancer@users.noreply.github.com>
2026-04-18 09:34:31 +0900

    IO completo
    desarrollo de IO

--- Batalla/src/batalla/IO.java
+++ Batalla/src/batalla/IO.java
@@ -4,6 +4,8 @@
  */
 package batalla;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author thlor
@@ -12,5 +14,32 @@ public class IO {
     // Atributos
     
     // Metodos
+    
+    public static void mostrar(String msg){
+        JOptionPane.showMessageDialog(null, msg);
+    }
+    
+    public static void mostrar(char msg){
+        JOptionPane.showMessageDialog(null, msg);
+    }
+    
+    public static int leerEntero(String msg) {
+        int entero = Integer.parseInt(JOptionPane.showInputDialog(msg));
+        return entero;
+    }
+    
+    public static double leerReal(String msg) {
+        Double real = Double.parseDouble(JOptionPane.showInputDialog(msg));
+        return real;
+    }
+    
+    public static String leerCadena(String msg){
+        String cadena = JOptionPane.showInputDialog(msg);
+        return cadena;
+    }
+    
+    public static char leerCaracter(String msg){
+        return JOptionPane.showInputDialog(msg).trim().toUpperCase().charAt(0);
+    }
     
 }
